36140129165afaa1a9b3b07367a132eb6a6e8fa3
MatheusSantos00 <80589583+MatheusSantos007@users.noreply.github.com>
2022-02-20 22:45:46 +0000

    Initial commit

---
+++ dataset.js
@@ -0,0 +1,22 @@
+const dataset = [
+    {
+        id: 1,
+        name:"Sorvete De Chocolate",
+        image:"img/sorvete1.jpg",
+        descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        preco:"10,00",
+
+    },
+    {   
+        id: 2,
+        name:"Sorvete De Morango",
+        image:"img/sorvete2.jpg",
+        descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        preco:"10,00",
+
+    },
+];
+
+
+export default dataset;
+  
